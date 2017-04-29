ee876539d8ba9be746ec4c2a1ca66fbf9bdd4dc0
lhongshuo@sina.cn <lhongshuo@sina.cn>
2017-04-29 12:22:38 +0800

    c

---
+++ application/admin/config.php
@@ -0,0 +1,10 @@
+<?php 
+return [
+    'app_namespace'          => 'admin',//把app改成admin
+    // 默认模块名
+ 
+    'default_controller'     => 'test',
+    // 默认操作名
+    'default_action'         => 'index',
+];
+?>
